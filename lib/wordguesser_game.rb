f8ec382c38063774b23bb49c9fe0edb10f332db7
Edward Kim <edwardkim0115@berkeley.edu>
2025-07-13 05:28:01 +0000

    hello

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,23 +1,34 @@
 class WordGuesserGame
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/wordguesser_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
+  attr_accessor :word, :guesses, :wrong_guesses
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
-  # You can test it by installing irb via $ gem install irb
-  # and then running $ irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
-  #  => "cooking"   <-- some random word
+  def guess(letter)
+    raise ArgumentError if letter.nil? || letter.empty? || letter =~ /[^a-zA-Z]/
+    letter = letter.downcase
+    return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+    @word.include?(letter) ? @guesses += letter : @wrong_guesses += letter
+    true
+  end
+
+  def word_with_guesses
+    @word.chars.map { |c| @guesses.include?(c) ? c : '-' }.join
+  end
+
+  def check_win_or_lose
+    return :win if word_with_guesses == @word
+    return :lose if @wrong_guesses.length >= 7
+    :play
+  end
+
   def self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://randomword.saasbook.info/RandomWord')
-    Net::HTTP.new('randomword.saasbook.info').start do |http|
-      return http.post(uri, "").body
-    end
+    Net::HTTP.new('randomword.saasbook.info').start { |http| return http.post(uri, "").body }
   end
 end
--- spec/wordguesser_game_spec.rb
+++ spec/wordguesser_game_spec.rb
@@ -2,78 +2,65 @@ require 'spec_helper'
 require 'wordguesser_game'
 
 describe WordGuesserGame do
-  # helper function: make several guesses
   def guess_several_letters(game, letters)
-    letters.chars do |letter|
-      game.guess(letter)
+    letters.chars { |letter| game.guess(letter) }
+  end
+
+  describe 'new' do
+    it "initializes game state" do
+      game = described_class.new('glorp')
+      expect(game.word).to eq('glorp')
+      expect(game.guesses).to eq('')
+      expect(game.wrong_guesses).to eq('')
     end
   end
 
-  describe 'new', pending: true do
-    it "takes a parameter and returns a WordGuesserGame object" do
-      @game = described_class.new('glorp')
-      expect(@game).to be_an_instance_of(described_class)
-      expect(@game.word).to eq('glorp')
-      expect(@game.guesses).to eq('')
-      expect(@game.wrong_guesses).to eq('')
-    end
-  end
-
-  describe 'guessing', pending: true do
+  describe 'guessing' do
     context 'correctly' do
-      before do
-        @game = described_class.new('garply')
-        @valid = @game.guess('a')
-      end
+      before { @game = described_class.new('garply'); @valid = @game.guess('a') }
 
-      it 'changes correct guess list', pending: true do
+      it 'updates correct guesses' do
         expect(@game.guesses).to eq('a')
         expect(@game.wrong_guesses).to eq('')
       end
 
-      it 'returns true', pending: true do
+      it 'returns true' do
         expect(@valid).not_to be false
       end
     end
 
     context 'incorrectly' do
-      before do
-        @game = described_class.new('garply')
-        @valid = @game.guess('z')
-      end
+      before { @game = described_class.new('garply'); @valid = @game.guess('z') }
 
-      it 'changes wrong guess list', pending: true do
+      it 'updates wrong guesses' do
         expect(@game.guesses).to eq('')
         expect(@game.wrong_guesses).to eq('z')
       end
 
-      it 'returns true', pending: true do
+      it 'returns true' do
         expect(@valid).not_to be false
       end
     end
 
     context 'same letter repeatedly' do
-      before do
-        @game = described_class.new('garply')
-        guess_several_letters(@game, 'aq')
-      end
+      before { @game = described_class.new('garply'); guess_several_letters(@game, 'aq') }
 
-      it 'does not change correct guess list', pending: true do
+      it 'ignores repeated correct guess' do
         @game.guess('a')
         expect(@game.guesses).to eq('a')
       end
 
-      it 'does not change wrong guess list', pending: true do
+      it 'ignores repeated wrong guess' do
         @game.guess('q')
         expect(@game.wrong_guesses).to eq('q')
       end
 
-      it 'returns false', pending: true do
+      it 'returns false for repeated guesses' do
         expect(@game.guess('a')).to be false
         expect(@game.guess('q')).to be false
       end
 
-      it 'is case insensitive', pending: true do
+      it 'is case insensitive' do
         expect(@game.guess('A')).to be false
         expect(@game.guess('Q')).to be false
         expect(@game.guesses).not_to include('A')
@@ -81,38 +68,24 @@ describe WordGuesserGame do
       end
     end
 
-    context 'invalid' do
-      before do
-        @game = described_class.new('foobar')
-      end
+    context 'invalid input' do
+      before { @game = described_class.new('foobar') }
 
-      it 'throws an error when empty', pending: true do
-        expect { @game.guess('') }.to raise_error(ArgumentError)
-      end
-
-      it 'throws an error when not a letter', pending: true do
-        expect { @game.guess('%') }.to raise_error(ArgumentError)
-      end
-
-      it 'throws an error when nil', pending: true do
-        expect { @game.guess(nil) }.to raise_error(ArgumentError)
-      end
+      it { expect { @game.guess('') }.to raise_error(ArgumentError) }
+      it { expect { @game.guess('%') }.to raise_error(ArgumentError) }
+      it { expect { @game.guess(nil) }.to raise_error(ArgumentError) }
     end
   end
 
-  describe 'displayed word with guesses', pending: true do
-    before do
-      @game = described_class.new('banana')
-    end
+  describe 'displayed word with guesses' do
+    before { @game = described_class.new('banana') }
 
-    # for a given set of guesses, what should the word look like?
-    @test_cases = {
+    {
       'bn' => 'b-n-n-',
       'def' => '------',
       'ban' => 'banana'
-    }
-    @test_cases.each_pair do |guesses, displayed|
-      it "is '#{displayed}' when guesses are '#{guesses}'" do
+    }.each do |guesses, displayed|
+      it "is '#{displayed}' for guesses '#{guesses}'" do
         guess_several_letters(@game, guesses)
         expect(@game.word_with_guesses).to eq(displayed)
       end
@@ -120,21 +93,19 @@ describe WordGuesserGame do
   end
 
   describe 'game status' do
-    before do
-      @game = described_class.new('dog')
-    end
+    before { @game = described_class.new('dog') }
 
-    it 'is win when all letters guessed', pending: true do
+    it 'detects win' do
       guess_several_letters(@game, 'ogd')
       expect(@game.check_win_or_lose).to eq(:win)
     end
 
-    it 'is lose after 7 incorrect guesses', pending: true do
+    it 'detects loss' do
       guess_several_letters(@game, 'tuvwxyz')
       expect(@game.check_win_or_lose).to eq(:lose)
     end
 
-    it 'continues play if neither win nor lose', pending: true do
+    it 'detects ongoing play' do
       guess_several_letters(@game, 'do')
       expect(@game.check_win_or_lose).to eq(:play)
     end
